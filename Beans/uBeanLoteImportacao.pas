82c67e9bd79b71b67dc26caf8e6d05c2b1c1375a
Sergio Jonas Schulz <sergioschu@gmail.com>
2015-12-28 19:36:31 -0200

    Ajustando Classe

--- Beans/uBeanLoteImportacao.pas
+++ Beans/uBeanLoteImportacao.pas
@@ -8,10 +8,37 @@ uses
 
 type
   TLOTEIMPORTACAO = class(TFWPersistence)
-    property ID : TFieldInteger;
-    property DATAHORA : TFieldDateTime;
+  private
+    FID: TFieldInteger;
+    FDATA_HORA: TFieldDateTime;
+    procedure SetDATA_HORA(const Value: TFieldDateTime);
+    procedure SetID(const Value: TFieldInteger);
+  protected
+    procedure InitInstance; override;
+  published
+    property ID : TFieldInteger read FID write SetID;
+    property DATA_HORA : TFieldDateTime read FDATA_HORA write SetDATA_HORA;
   end;
 
 implementation
+
+{ TLOTEIMPORTACAO }
+
+procedure TLOTEIMPORTACAO.InitInstance;
+begin
+  inherited;
+  ID.isPK             := True;
+  DATA_HORA.isNotNull := True;
+end;
+
+procedure TLOTEIMPORTACAO.SetDATA_HORA(const Value: TFieldDateTime);
+begin
+  FDATA_HORA := Value;
+end;
+
+procedure TLOTEIMPORTACAO.SetID(const Value: TFieldInteger);
+begin
+  FID := Value;
+end;
 
 end.
